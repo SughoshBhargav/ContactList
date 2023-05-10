736e8ffd08b682d4ba7dd6d0402b7819936ab5e4
SughoshBhargav <sughoshsv@.com>
2023-05-10 16:10:03 +0530

    Updates

---
+++ precache-manifest.fbe10e9b159e3675b2f9d360a7047182.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a342475513c682d7d5690f5e683c2839",
+    "url": "/ContactList/index.html"
+  },
+  {
+    "revision": "db6d7cd0e9953020b0a6",
+    "url": "/ContactList/static/js/2.ad00698b.chunk.js"
+  },
+  {
+    "revision": "1953c979a57154331ab5",
+    "url": "/ContactList/static/js/main.c9c70682.chunk.js"
+  },
+  {
+    "revision": "fb3d6cb579b166107e26",
+    "url": "/ContactList/static/js/runtime-main.aa94a6d1.js"
+  }
+]);
